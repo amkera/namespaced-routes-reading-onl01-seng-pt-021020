e83ef1dae98014192db1e33b5a005f1ddfd29282
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-07-11 02:45:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,18 @@ Rails.application.routes.draw do
 
 
   
-  scope '/admin', module: 'admin' do
-    resources :stats, only: [:index]
-  end
+  #scope '/admin', module: 'admin' do
+    #resources :stats, only: [:index]
+  #end
+  
   #creates stats_path and shows the index page 
   #We're telling scope that we want to use /admin as a URL prefix, and we're also letting Rails know that all of the included routes will be handled by controllers in the admin module.
+  
+  #Fortunately, Rails gives us a shortcut here. When we want to route with a module and use that module's name as the URL prefix, we can use the namespace method instead of scope, module.
+  
+  namespace :admin do 
+    resources :stats, only: [:index]
+  end 
   
 
   root 'posts#index'
