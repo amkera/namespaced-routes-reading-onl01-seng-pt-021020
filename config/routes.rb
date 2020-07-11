901680274f8b834b0354bab5bf472c327723f40d
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-07-11 02:41:48 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     resources :stats, only: [:index]
   end
   #creates stats_path and shows the index page 
+  #We're telling scope that we want to use /admin as a URL prefix, and we're also letting Rails know that all of the included routes will be handled by controllers in the admin module.
   
 
   root 'posts#index'
